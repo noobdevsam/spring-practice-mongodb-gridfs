4020112c40e855cac1191fee8f7f3359f1ce3a1d
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-23 19:53:01 +0600

    refactor DocumentMetadataRepository to use String id instead of UUID

--- src/main/java/com/example/springpracticemongodbgridfs/repos/DocumentMetadataRepository.java
+++ src/main/java/com/example/springpracticemongodbgridfs/repos/DocumentMetadataRepository.java
@@ -3,7 +3,5 @@ package com.example.springpracticemongodbgridfs.repos;
 import com.example.springpracticemongodbgridfs.model.DocumentMetadata;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.UUID;
-
-public interface DocumentMetadataRepository extends MongoRepository<DocumentMetadata, UUID> {
+public interface DocumentMetadataRepository extends MongoRepository<DocumentMetadata, String> {
 }
