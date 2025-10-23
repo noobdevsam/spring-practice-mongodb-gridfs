2f639086f88cb69c7946bdc58dfdd664b21d134a
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-23 18:38:23 +0600

    initial commit

---
+++ src/main/java/com/example/springpracticemongodbgridfs/SpringPracticeMongodbGridfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticemongodbgridfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeMongodbGridfsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringPracticeMongodbGridfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticemongodbgridfs/SpringPracticeMongodbGridfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticemongodbgridfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeMongodbGridfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
